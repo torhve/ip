2bc366972f5739c0e7eddaab28887d9133c4da24
Tor Hveem <tor@hveem.no>
2015-06-05 15:58:44 +0200

    add geoip

--- ip.lua
+++ ip.lua
@@ -13,7 +13,25 @@ local vars = {
   post_args = ngx.req.get_post_args(),
   port = ngx.var.remote_port,
   uri = ngx.var.request_uri,
-  scheme = ngx.var.scheme
+  scheme = ngx.var.scheme,
+  geo_vars = {
+    country_code = ngx.var.geoip_country_code,
+    country_code3 = ngx.var.geoip_country_code3,
+    country = ngx.var.geoip_country,
+    region = ngx.var.geoip_region,
+    region_code = ngx.var.geoip_region_code,
+    city = ngx.var.geoip_city,
+    postal_code = ngx.var.geoip_postal_code,
+    continent_code = ngx.var.geoip_continent_code,
+    latitude = ngx.var.geoip_latitude,
+    longitude = ngx.var.geoip_longitude,
+    dma_code = ngx.var.geoip_dma_code,
+    area_code = ngx.var.geoip_area_code,
+    timezone = ngx.var.geoip_timezone,
+    offset = ngx.var.geoip_offset,
+    asn = ngx.var.asn,
+    isp = ngx.var.isp
+  }
 }
 if vars.headers.accept and vars.headers.accept:match('application/json') then
   ngx.header.content_type = 'application/json'
